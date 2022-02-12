bc86f5c6296fc2f72315681dc3d850569e586104
Roman Schavala <romastal915@gmail.com>
2022-02-12 23:14:06 +0200

    ready project

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+
 document.addEventListener('DOMContentLoaded', function() {
+  // navbar 
+  // бокове меню працює, але немає значка гамбургера 
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, {});
+});
+
+// carousel 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {
+  });
+});
+
+// zoom images 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, {});
 });
